af99fe654b9a32124cd233137face2ade4cfbc31
RAJAT SHARMA <rajat.sharma2727@gmail.com>
2023-02-15 12:28:19 +0530

    remote

--- src/main/java/com/example/NestedData.java
+++ src/main/java/com/example/NestedData.java
@@ -1,16 +1,14 @@
 package com.example;
 
-
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
-import static java.lang.System.out;
-
-class BuildCombination {
+class NestedData {
     static LinkedHashMap<String, List<String>> map = new LinkedHashMap<>();
     static List<String[]> result = new ArrayList<>();
-    public static final List<List<String>> addEx = new ArrayList<>();
+    public static final List<String> path = new ArrayList<>();
 
     public static void main(String[] args) {
         String[][] input = {
@@ -23,7 +21,7 @@ class BuildCombination {
                 {"3", "door", "handle", "proc"},
                 {"4", "window", "glass", "proc"},
                 {"5", "tyre", "rubber", "proc"},
-                };
+        };
 
         for (String[] row : input) {
             if (row[3].equals("mfg")) {
@@ -34,76 +32,75 @@ class BuildCombination {
             }
         }
 
-        List<String> path = new ArrayList<>();
-        mappingTheParentNode("car", path, input);
         createHeaders();
-        out.println();
-        printLevels();
+        System.out.println();
+        for (Map.Entry<String, List<String>> stringListEntry : map.entrySet()) {
+            if (!stringListEntry.getKey().equals("door"))
+                mappingTheParentNode(stringListEntry.getKey(), path, input);
+        }
     }
 
     private static void createHeaders() {
         StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("Combination number"+"\t  ");
+        stringBuilder.append("Combination number" + "\t  ");
         int maxValue = 0;
-        for (List<String> array : addEx) {
+        for (List<String> array : map.values()) {
             int value = array.size();
             if (value > maxValue) {
                 maxValue = value;
             }
         }
-        maxValue=maxValue/2;
+        maxValue = maxValue / 2;
         for (int i = 1; i <= maxValue; i++) {
             stringBuilder.append("level").append(i).append("  ").append("level").append(i).append(" id").append("  ");
         }
-        out.print(stringBuilder);
+        System.out.print(stringBuilder);
     }
 
     private static void mappingTheParentNode(String node, List<String> path, String[][] input) {
         path.add(node);
         path.add(getId(input, node));
-        makeItFormat(path);
-        // out.println(path);
+
         if (!map.containsKey(node)) {
-            result.add(path.toArray(new String[0]));
+            printCombination();
+            path.remove(path.size() - 1);
+            path.remove(path.size() - 1);
             return;
         }
         for (String next : map.get(node)) {
             List<String> newPath = new ArrayList<>(path);
             mappingTheParentNode(next, newPath, input);
         }
+        path.remove(path.size() - 1);
+        path.remove(path.size() - 1);
     }
 
-    private static void makeItFormat(List<String> path) {
-        addEx.add(path);
+    private static void printCombination() {
+        int count = result.size() + 1;
+        String[] arr = path.toArray(new String[0]);
+        String[] newComb = new String[arr.length + 1];
+        newComb[0] = Integer.toString(count);
+        for (int i = 0; i < arr.length; i++) {
+            newComb[i + 1] = arr[i];
+        }
+        result.add(newComb);
+        System.out.print("\t\t" + count + "\t\t\t");
+        for (String s : newComb) {
+            System.out.print("  " + s + "\t  ");
+        }
+        System.out.println();
     }
 
-    private static String getId(String[][] input, String item) {
-        for (String[] in : input) {
-            if (in[1].equals(item)) {
-                return in[0];
+    private static String getId(String[][] input, String node) {
+        for (String[] row : input) {
+            if (row[1].equals(node)) {
+                return row[0];
             }
         }
         return "";
     }
 
-    private static void printLevels() {
-        int count = 1;
-        for (List<String> arr : addEx) {
-            out.print("\t\t"+count + "\t\t\t");
-            count++;
-            for (String s : arr) {
-                out.print("  "+s + "\t  ");
-            }
-            out.println();
-        }
-        for (int i = 0; i < addEx.size(); i++) {
-            out.print("\t\t"+count + "\t\t\t");
-            for (String s : addEx.get(i)) {
-                out.print("  "+s + "\t  ");
-            }
-            out.println();
-        }
-    }
+
 }
 
 
